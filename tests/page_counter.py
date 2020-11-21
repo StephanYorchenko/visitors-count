fdb40aef4f6e2adf817368590fb0271a9662b118
Stepan Yurchenko <stepan33314@yandex.ru>
2020-11-21 23:56:37 +0500

    Add tests

---
+++ tests/page_counter.py
@@ -0,0 +1,20 @@
+import unittest
+from routes.domain import PageStatistics
+
+
+class MyTestCase(unittest.TestCase):
+	def setUp(self):
+		self.page_stat = PageStatistics()
+
+	def test_initialize(self):
+		self.assertEqual(self.page_stat.count, 0)
+		self.assertEqual(self.page_stat.visitors, set())
+
+	def test_visit(self):
+		self.page_stat.visit(12345)
+		self.assertEqual(self.page_stat.count, 1)
+		self.assertTrue(12345 in self.page_stat.visitors)
+
+
+if __name__ == '__main__':
+	unittest.main()
